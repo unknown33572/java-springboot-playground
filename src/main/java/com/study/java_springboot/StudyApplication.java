6945086b124f657e1c1a255f24d953d58a02329a
S <s@MacBook-Pro.local>
2026-02-23 21:17:36 +0900

    Initial commit

---
+++ src/main/java/com/study/java_springboot/StudyApplication.java
@@ -0,0 +1,13 @@
+package com.study.java_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(StudyApplication.class, args);
+  }
+
+}
